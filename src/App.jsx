aeb6296d30318199e79f9abfe33cea7b4c801e00
Sam <s_holleworth@outlook.com>
2021-09-23 17:43:09 +0100

    Removed some golbals

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,40 @@ import MainTimer from './components/MainTimer'
 import Buttons from './components/Buttons'
 import Laps from './components/Laps'
 
-let mainStartTime = 0
-let lapStartTime = 0
-let mainSavedTime = 0
-let lapSavedTime = 0
 let slowestLap = -Infinity
 let fastestLap = Infinity
-let intervalId = 0
+
+const getElapsedTimeInMilliseconds = (startTime) => {
+  return Date.now() - startTime;
+}
 
 const App = () => {
   const [started, setStarted] = useState(false)
   const [running, setRunning] = useState(false)
+  const [activeLapTime, setActiveLapTime] = useState(0)
   const [mainTime, setMainTime] = useState(0)
   const [lapTimes, setLapTimes] = useState([])
-  const [activeLapTime, setActiveLapTime] = useState(0)
-
+  
+  useEffect(() => {
+    const lapStartTime = Date.now() - activeLapTime
+    if(running) {
+      const mainStartTime = Date.now() - mainTime
+      const intervalId = setInterval(() => { runTimer(mainStartTime, lapStartTime) }, 16)
+      return () => clearInterval(intervalId)
+    }
+  },[running, lapTimes])
+  
   const start = () => {
-    mainStartTime = Date.now()
-    lapStartTime = Date.now()
     setRunning(true)
-    runTimer()
     if(!started)
       setStarted(true)
   }
 
   const stop = () => {
-    mainSavedTime = mainTime
-    lapSavedTime = activeLapTime
     setRunning(false)
-    stopTimer()
   }
 
   const reset = () => {
-    mainSavedTime = 0
-    lapSavedTime = 0
     slowestLap = -Infinity
     fastestLap = Infinity
     setLapTimes([])
@@ -49,9 +49,7 @@ const App = () => {
   const makeLap = () => {
     updateFastAndSlowLap(activeLapTime)
     setLapTimes([activeLapTime, ...lapTimes])
-    setActiveLapTime(0)
-    lapStartTime = Date.now()
-    lapSavedTime = 0
+    setActiveLapTime(0)  
   }
 
   const updateFastAndSlowLap = (lapTime) => {
@@ -59,24 +57,10 @@ const App = () => {
     fastestLap = Math.min(lapTime, fastestLap)
   }
 
-  const getElapsedMainTimeInMilliseconds = () => {
-    return Date.now() + mainSavedTime - mainStartTime;
+  const runTimer = (mainStartTime, lapStartTime) => {
+    setMainTime(getElapsedTimeInMilliseconds(mainStartTime))
+    setActiveLapTime(getElapsedTimeInMilliseconds(lapStartTime))
   }
-
-  const getElapsedLapTimeInMilliseconds = () => {
-    return Date.now() + lapSavedTime - lapStartTime;
-  }
-
-  const runTimer = () => {
-    setMainTime(getElapsedMainTimeInMilliseconds())
-    setActiveLapTime(getElapsedLapTimeInMilliseconds())
-  }
-
-  useEffect(() => {
-    running ? intervalId = setInterval(() => {
-      runTimer()
-    }, 16) : clearInterval(intervalId)
-  },[running])
 
   return (
     <div className="App">
